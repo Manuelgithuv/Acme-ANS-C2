21e1d8beb8c0eea9180fb052b3ac84b3df7b68da
Mancalrod <mancalrod@alum.us.es>
2025-03-25 10:01:17 +0100

    Arreglando relaciones n to n

---
+++ src/main/java/acme/entities/booking/BookingPassenger.java
@@ -0,0 +1,34 @@
+
+package acme.entities.booking;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.validation.Mandatory;
+import acme.entities.passenger.Passenger;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class BookingPassenger extends AbstractEntity {
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Relationships ----------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Booking				Booking;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = true)
+	private Passenger			Passenger;
+
+}
--- src/main/java/acme/entities/passenger/Passenger.java
+++ src/main/java/acme/entities/passenger/Passenger.java
@@ -4,10 +4,8 @@ package acme.entities.passenger;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
@@ -18,7 +16,6 @@ import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
 import acme.constraints.ValidLongText;
 import acme.constraints.ValidShortText;
-import acme.entities.booking.Booking;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -54,11 +51,4 @@ public class Passenger extends AbstractEntity {
 	@Automapped
 	private String				specialNeeds;
 
-	// Relaciones
-	// -------------------------------------------------------------------
-
-	@Mandatory
-	@Valid
-	@ManyToOne(optional = false)
-	private Booking				booking;
 }
