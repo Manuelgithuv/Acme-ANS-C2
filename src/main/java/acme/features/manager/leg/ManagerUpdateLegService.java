557dc6ccaaedfcc962f8e311b58b5cc4e613260b
Manuel Artero <manuel.artero04@gmail.com>
2025-04-02 18:57:31 +0200

    Algunos arreglos en legs

--- src/main/java/acme/features/manager/leg/ManagerUpdateLegService.java
+++ src/main/java/acme/features/manager/leg/ManagerUpdateLegService.java
@@ -2,13 +2,16 @@
 package acme.features.manager.leg;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import acme.client.components.models.Dataset;
 import acme.client.components.views.SelectChoices;
+import acme.client.helpers.MomentHelper;
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
 import acme.datatypes.LegStatus;
@@ -121,8 +124,50 @@ public class ManagerUpdateLegService extends AbstractGuiService<Manager, Leg> {
 
 		if (areAirportsEquals)
 			super.state(false, "*", "manager.leg.create.airports");
+		if (leg.getFlight() != null) {
+			boolean res = this.validateTimeAndAirportsInConsecutiveLegs(leg);
+			super.state(res, "*", "manager.consecutive.legs.invalid.dates-or-airports");
+		}
+		if (leg.getScheduledDeparture() != null) {
+			long actualUpperLimit = MomentHelper.getCurrentMoment().getTime() / 60000;
+			long departureInMinutes = leg.getScheduledDeparture().getTime() / 60000;
+			if (departureInMinutes < actualUpperLimit)
+				super.state(false, "scheduledDeparture", "departure.minimum.currentDate");
+		}
 
 		super.state(status, "*", "manager.leg.create.dates");
+	}
+
+	private boolean validateTimeAndAirportsInConsecutiveLegs(final Leg leg) {
+
+		boolean res = false;
+		List<Leg> legs = this.legRepository.findDistinctByFlight(leg.getFlight().getId());
+
+		if (leg.getScheduledDeparture() != null && leg.getScheduledArrival() != null && leg.getArrivalAirport() != null && leg.getDepartureAirport() != null) {
+			for (int i = 0; i < legs.size(); i++)
+				if (legs.get(i).getId() == leg.getId()) {
+					legs.set(i, leg);
+					break;
+				}
+			legs.sort(Comparator.comparing(Leg::getScheduledDeparture));
+			for (int i = 0; i < legs.size() - 1; i++) {
+
+				Leg currentLeg = legs.get(i);
+				Leg nextLeg = legs.get(i + 1);
+
+				long currentArrivalInMinutes = currentLeg.getScheduledArrival().getTime() / 60000;
+				long nextDepartureInMinutes = nextLeg.getScheduledDeparture().getTime() / 60000;
+
+				Airport currentAirport = currentLeg.getArrivalAirport();
+				Airport nextAirport = nextLeg.getDepartureAirport();
+
+				if (currentArrivalInMinutes >= nextDepartureInMinutes || currentAirport.getId() != nextAirport.getId())
+					res = false;
+				else
+					res = true;
+			}
+		}
+		return res;
 	}
 
 	@Override
