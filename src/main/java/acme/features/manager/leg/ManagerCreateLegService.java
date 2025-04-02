30d90f23091150a949f27bdc095329651a60cafd
Manuelgithuv <manuel.artero04@gmail.com>
2025-04-02 20:10:48 +0200

    doc de analisis

--- src/main/java/acme/features/manager/leg/ManagerCreateLegService.java
+++ src/main/java/acme/features/manager/leg/ManagerCreateLegService.java
@@ -140,8 +140,8 @@ public class ManagerCreateLegService extends AbstractGuiService<Manager, Leg> {
 		if (areAirportsEquals)
 			super.state(false, "*", "manager.leg.create.airports");
 		if (leg.getFlight() != null) {
-			boolean res = this.validateTimeAndAirportsInConsecutiveLegs(leg);
-			super.state(res, "*", "manager.consecutive.legs.invalid.dates-or-airports");
+			boolean res = this.validateTimeInConsecutiveLegs(leg);
+			super.state(res, "*", "manager.consecutive.legs.invalid.dates");
 		}
 		if (leg.getScheduledDeparture() != null) {
 			long actualUpperLimit = MomentHelper.getCurrentMoment().getTime() / 60000;
@@ -153,7 +153,7 @@ public class ManagerCreateLegService extends AbstractGuiService<Manager, Leg> {
 		super.state(status, "*", "manager.leg.create.dates");
 	}
 
-	private boolean validateTimeAndAirportsInConsecutiveLegs(final Leg leg) {
+	private boolean validateTimeInConsecutiveLegs(final Leg leg) {
 
 		boolean res = false;
 		List<Leg> legs = this.legRepository.findDistinctByFlight(leg.getFlight().getId());
@@ -168,11 +168,12 @@ public class ManagerCreateLegService extends AbstractGuiService<Manager, Leg> {
 
 				long currentArrivalInMinutes = currentLeg.getScheduledArrival().getTime() / 60000;
 				long nextDepartureInMinutes = nextLeg.getScheduledDeparture().getTime() / 60000;
+				
+				long currentDepartureInMinutes = currentLeg.getScheduledDeparture().getTime() / 60000;
+				long nextArrivalInMinutes =  nextLeg.getScheduledArrival().getTime() / 60000;
 
-				Airport currentAirport = currentLeg.getArrivalAirport();
-				Airport nextAirport = nextLeg.getDepartureAirport();
 
-				if (currentArrivalInMinutes >= nextDepartureInMinutes || currentAirport.getId() != nextAirport.getId())
+				if (currentArrivalInMinutes >= nextDepartureInMinutes || currentDepartureInMinutes==nextDepartureInMinutes || currentArrivalInMinutes==nextArrivalInMinutes)
 					res = false;
 				else
 					res = true;
--- src/main/java/acme/features/manager/leg/ManagerUpdateLegService.java
+++ src/main/java/acme/features/manager/leg/ManagerUpdateLegService.java
@@ -125,8 +125,8 @@ public class ManagerUpdateLegService extends AbstractGuiService<Manager, Leg> {
 		if (areAirportsEquals)
 			super.state(false, "*", "manager.leg.create.airports");
 		if (leg.getFlight() != null) {
-			boolean res = this.validateTimeAndAirportsInConsecutiveLegs(leg);
-			super.state(res, "*", "manager.consecutive.legs.invalid.dates-or-airports");
+			boolean res = this.validateTimeInConsecutiveLegs(leg);
+			super.state(res, "*", "manager.consecutive.legs.invalid.dates");
 		}
 		if (leg.getScheduledDeparture() != null) {
 			long actualUpperLimit = MomentHelper.getCurrentMoment().getTime() / 60000;
@@ -138,7 +138,7 @@ public class ManagerUpdateLegService extends AbstractGuiService<Manager, Leg> {
 		super.state(status, "*", "manager.leg.create.dates");
 	}
 
-	private boolean validateTimeAndAirportsInConsecutiveLegs(final Leg leg) {
+	private boolean validateTimeInConsecutiveLegs(final Leg leg) {
 
 		boolean res = false;
 		List<Leg> legs = this.legRepository.findDistinctByFlight(leg.getFlight().getId());
@@ -157,11 +157,12 @@ public class ManagerUpdateLegService extends AbstractGuiService<Manager, Leg> {
 
 				long currentArrivalInMinutes = currentLeg.getScheduledArrival().getTime() / 60000;
 				long nextDepartureInMinutes = nextLeg.getScheduledDeparture().getTime() / 60000;
+				
+				long currentDepartureInMinutes = currentLeg.getScheduledDeparture().getTime() / 60000;
+				long nextArrivalInMinutes =  nextLeg.getScheduledArrival().getTime() / 60000;
 
-				Airport currentAirport = currentLeg.getArrivalAirport();
-				Airport nextAirport = nextLeg.getDepartureAirport();
 
-				if (currentArrivalInMinutes >= nextDepartureInMinutes || currentAirport.getId() != nextAirport.getId())
+				if (currentArrivalInMinutes >= nextDepartureInMinutes || currentDepartureInMinutes==nextDepartureInMinutes || currentArrivalInMinutes==nextArrivalInMinutes)
 					res = false;
 				else
 					res = true;
